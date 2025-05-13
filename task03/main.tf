61db8140e173d9d6495ed0cf0adf6afe35e91b9c
SanjanaSingh1999 <60661678+SanjanaSingh1999@users.noreply.github.com>
2025-05-13 10:25:07 +0530

    Update main.tf

--- task03/main.tf
+++ task03/main.tf
@@ -1,18 +1,19 @@
-# Reference the existing resource group
+# Import existing resource group
 data "azurerm_resource_group" "existing" {
   name = var.rg_name
 }
 
-# Update tags on the existing resource group
+# Update existing RG with tag
 resource "azurerm_resource_group" "this" {
   name     = data.azurerm_resource_group.existing.name
   location = data.azurerm_resource_group.existing.location
 
   tags = {
-    Creator = var.student_email
+    Creator = var.creator_tag
   }
 }
 
+# Storage account
 resource "azurerm_storage_account" "this" {
   name                     = var.storageaccount_name
   resource_group_name      = azurerm_resource_group.this.name
@@ -21,29 +22,32 @@ resource "azurerm_storage_account" "this" {
   account_replication_type = "LRS"
 
   tags = {
-    Creator = var.student_email
+    Creator = var.creator_tag
   }
 }
 
+# Virtual network
 resource "azurerm_virtual_network" "this" {
   name                = var.vnet_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
   address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 
   tags = {
-    Creator = var.student_email
+    Creator = var.creator_tag
   }
 }
 
-resource "azurerm_subnet" "subnet1" {
+# Subnet 1 - frontend
+resource "azurerm_subnet" "frontend" {
   name                 = var.subnet1_name
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_subnet" "subnet2" {
+# Subnet 2 - backend
+resource "azurerm_subnet" "backend" {
   name                 = var.subnet2_name
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
