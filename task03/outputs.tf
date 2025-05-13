c89704d444dcfdb5d29d76c01ad6490282e3b9b6
SanjanaSingh1999 <60661678+SanjanaSingh1999@users.noreply.github.com>
2025-05-13 10:20:56 +0530

    Update outputs.tf

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,11 +1,14 @@
 output "rg_id" {
-  value = azurerm_resource_group.this.id
+  value       = azurerm_resource_group.this.id
+  description = "The ID of the Azure Resource Group."
 }
 
 output "sa_blob_endpoint" {
-  value = azurerm_storage_account.this.primary_blob_endpoint
+  value       = azurerm_storage_account.this.primary_blob_endpoint
+  description = "The primary blob service endpoint of the Azure Storage Account."
 }
 
 output "vnet_id" {
-  value = azurerm_virtual_network.this.id
+  value       = azurerm_virtual_network.this.id
+  description = "The ID of the Azure Virtual Network."
 }
