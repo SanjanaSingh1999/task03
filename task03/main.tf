dec0938c93eeec7c289edb1855f0b2f8568277ce
SanjanaSingh1999 <60661678+SanjanaSingh1999@users.noreply.github.com>
2025-05-13 10:32:47 +0530

    Update main.tf

--- task03/main.tf
+++ task03/main.tf
@@ -20,9 +20,6 @@ resource "azurerm_storage_account" "this" {
   location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  
-  # Ensure blob public access is disabled
-  allow_blob_public_access = false
 
   tags = {
     Creator = var.creator_tag
